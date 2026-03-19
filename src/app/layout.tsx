18621f16f1535a07a42c26cb3d3461ba21a95cbb
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2026-03-19 16:34:32 +0700

    Update the metadata for the seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import { sans } from '@/lib/fonts'
+import type { Metadata } from 'next'
 import './globals.css'
 
-export const metadata = {
+export const metadata: Metadata = {
 	metadataBase: new URL('https://caeser-chocolate.vercel.app'),
 	title: 'Caeser Chocolate',
 	description: 'Luxury chocolate brand landing page by Caeser Ibrahim.',
